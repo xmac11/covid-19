4502b0835458edce7b0cd151d14b827a224ed2f1
Charalampos Makrylakis <c.makrylakis@gmail.com>
2020-05-24 04:59:08 +0300

    Added credits for build script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Credits: https://github.com/TQueV/covid-19
 # This script is based on https://github.com/harisont/covid-19/commit/f3ff43e1ec1aa7fa9648b7253cdd199374ad713f
 # See https://github.com/harisont/covid-19/issues/2#issuecomment-623906520 for discussion
 pandoc --from markdown_strict+footnotes words/words.md -o words/words.html
